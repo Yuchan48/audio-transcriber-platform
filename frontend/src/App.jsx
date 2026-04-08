53fa4143d900e26cbfc03e72e204dac98c53f2b5
Yuchan48 <yuchan.iizuka@gmail.com>
2026-04-08 21:28:55 +0200

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
-  return <div className="text-red-700">hello</div>;
-}
+// Import pages
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import RegisterPage from "./pages/RegisterPage";
+
+const App = () => {
+  return (
+    <div className="App bg-gray-300 min-h-screen w-full flex items-center justify-center">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;

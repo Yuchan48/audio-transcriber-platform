015d4bdfc571c6aea7aa7b22f8566bdf43d98349
Yuchan48 <yuchan.iizuka@gmail.com>
2026-04-12 00:04:49 +0200

    Prevents row click when delete button is pressed. It was triggering fetch audio file function right after calling delete audio function for the same file, resulting in error

--- frontend/src/components/buttons/DeleteButton.jsx
+++ frontend/src/components/buttons/DeleteButton.jsx
@@ -1,7 +1,10 @@
 const DeleteButton = ({ onClick, disabled }) => {
   return (
     <button
-      onClick={onClick}
+      onClick={(e) => {
+        e.stopPropagation(); // prevent row click
+        onClick();
+      }}
       className={`border border-red-500 text-red-500 px-2 py-1 rounded font-bold text-sm ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-red-100 transition"}`}
       disabled={disabled}
     >

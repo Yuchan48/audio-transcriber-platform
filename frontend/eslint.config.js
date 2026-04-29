8c83a4d0f9a421ca5161e77076562361cb64b94e
Yuchan48 <yuchan.iizuka@gmail.com>
2026-04-29 10:53:44 +0200

    typescript migration initial setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,15 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [
       js.configs.recommended,
+      // TypeScript support (IMPORTANT)
+      ...tseslint.configs.recommended,
+
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -17,13 +21,15 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+
+      // "@typescript-eslint/no-unused-vars": ["error"],
     },
   },
-])
+]);
